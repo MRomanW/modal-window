26b24123b7940530813f3e623a0d7d8e86f2af55
MRomanW <romanmelnyk21@gmail.com>
2023-09-14 07:30:16 +0300

    modal window

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const modalWindow = document.querySelector('.modal-window');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal-window');
+const bntsShowModalWindow = document.querySelectorAll('.show-modal-window');
+
+function hiddenClass(value) {
+  if (value === 'add') {
+    modalWindow.classList.add('hidden');
+    overlay.classList.add('hidden');
+  } else if (value === 'remove') {
+    modalWindow.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  }
+}
+
+for(let i = 0; i < bntsShowModalWindow.length; i++) {
+  bntsShowModalWindow[i].addEventListener('click', () => hiddenClass('remove'));
+}
+
+btnCloseModal.addEventListener('click', () => hiddenClass('add'));
+overlay.addEventListener('click', () => hiddenClass('add'));
+
+document.addEventListener('keydown', (e) => {
+  if(e.key === 'Escape' && !modalWindow.classList.contains('hidden')) {
+    hiddenClass('add');
+  }
+}) 
